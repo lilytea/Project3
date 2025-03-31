fef62c9302df5be360b4b4f95a1b12094ebfffe1
lilytea <julia.peng66@gmail.com>
2025-03-31 07:30:01 -0700

    Update client3.js

--- client3.js
+++ client3.js
@@ -19,7 +19,7 @@ let moveBackward = false;
 let moveLeft = false;
 let moveRight = false;
 let canJump = false;
-
+//
 let prevTime = performance.now();
 const velocity = new THREE.Vector3();
 const direction = new THREE.Vector3();
